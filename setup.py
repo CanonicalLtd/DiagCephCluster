a834b4a9f4d7a8b31ad6977f582bcc0837dca8a7
Kumar Rishabh <shailrishabh@gmail.com>
2016-06-12 03:05:17 +0000

    Corrected tests in setup.py
    tests can be now run via CLI(python setup.py test)

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 import sys
 
 desc = 'CLI script to diagnose a broken/partially working ceph cluster'
+test_module = 'nose.collector'
 
 setup(
     name='diagnose_ceph',
@@ -19,5 +20,5 @@ setup(
     long_description=open("README.md").read(),
     install_requires=['paramiko', 'pycodestyle', 'unittest2',
                       'nose', ],
-    test_suite='nosetests -s',
+    test_suite=test_module,
 )
